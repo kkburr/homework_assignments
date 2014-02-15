a8ef1203b6188e39391e124df5dbe6279ce79b05
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-02-15 08:03:23 -0700

    made homework executable

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -2,19 +2,8 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
 
-fizz_buzz = []
-numbers.each {|x| do
+first_array = numbers.map { |number| (number % 3 == 0 && number % 5 == 0) ? number = 'FizzBuzz' : number }
+second_array = first_array.map { |number| (number % 3 == 0) ? number = 'Fizz' : number }
+fizz_buzz = second_array.map { |number| (number % 5 == 0) ? number = 'Buzz' : number }
+puts fizz_buzz
 
-
-def fizz_buzz = ()
-if x % 5 != 0 && x % 3 != 0
-    number_var = x
-  elsif x % 3 == 0 && x % 5 == 0
-    number_var = 'FizzBuzz'
-  elsif x % 3 == 0
-    number_var = 'Fizz'
-  else x % 5 == 0
-  number_var = 'Buzz'
-  end
-  puts number_var
-end
