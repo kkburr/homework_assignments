94091b69d705973f0a12d8f8fc16b383ba5dde7a
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-02-15 08:12:22 -0700

    Fixed error

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -7,7 +7,7 @@ q = '\''
 puts string_practice
 puts string_practice.capitalize
 puts string_practice.upcase
-puts string_practice.insert(10, '\'').insert(17, '\'')
+puts string_practice.insert(10, '\'').insert(17, '\'').capitalize
 new_string_practice = string_practice.delete('\'')
 puts 'The string \''+ new_string_practice + '\' has ' + new_string_practice.length.to_s + ' characters'
 puts new_string_practice.reverse
